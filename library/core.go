3eb7b3c0a3b5d09a914ff9e3fae72a035833698f
Roman Khachko <romankhachko@gmail.com>
2021-10-11 15:13:35 -0500

    Squashed commit of the following:
    commit ece8b0ca741bf4abd8f98c58d96652a553c3f7d3
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 15:10:47 2021 -0500
    
        Update test covering realtime stdout. Use a shell script instead of relying on syslog tailing
    
    commit f4187f59d6f1a502e0332472ffcb525acfdc757f
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 14:56:16 2021 -0500
    
        Add library tests
    
    commit 8592cec5e9423b0e9c8d58435c289e31d321458e
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 13:00:37 2021 -0500
    
        Update GetJobStatus implementation to include exit code converted into string"
    
    commit f6a2c1b17a7657cf9b355c975ea57a3c6b3fc068
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 12:08:43 2021 -0500
    
        Update constants name according to Go style
    
    commit 50b5446d733958609773e0aea61c3ea55bef13ab
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 12:02:34 2021 -0500
    
        Add implementation of the default library API including user and jobs in-memory storage
    
    commit 865f18d4aba842fdbaea1c4a1e7285d71e57fdd3
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 01:25:29 2021 -0500
    
        Introduce default api of the library
    
    commit ac6eb158f591f1fc4945db9487a27d618e8240f8
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Mon Oct 11 00:32:27 2021 -0500
    
        Enhance output capturing for both stdout and stderr
    
    commit 7f90753895bf423ebb5e72c330709ff6fcbcfbd6
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Sun Oct 10 23:44:37 2021 -0500
    
        Enhance synchonization for the core library
    
    commit bde66e5c90bedf9ef9b4ffaea92e25928d3af219
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Sun Oct 10 20:23:50 2021 -0500
    
        Add other library capabilities: stop and stream the output
    
    commit 31899b92b378433c49a6cd16e47e70306bf5727c
    Author: Roman Khachko <romankhachko@gmail.com>
    Date:   Sat Oct 9 23:23:04 2021 -0500
    
        Add library structure and implement process start

---
+++ library/core.go
@@ -0,0 +1,191 @@
+package library
+
+/*
+This file contains the core of the library. It can perform the main manipulations with jobs (start/stop/stream output).
+If you need in-memory storage of created jobs, users and jobs accessed by users, please use default API that wraps core capabilities of the library.
+Default API can be found here: /library/default_api.go
+*/
+
+import (
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+)
+
+const (
+	InProgress    = "InProgress"
+	Exited        = "Exited"
+	Terminated    = "Terminated"
+	ExitedWithErr = "ExitedWithErr"
+)
+
+type Job struct {
+	ID        uuid.UUID
+	Process   *os.Process
+	Output    ByteSlice
+	OutputErr ByteSlice
+	ExitCode  int
+	JobStatus JobStatus
+}
+
+type JobStatus struct {
+	mutex     sync.RWMutex
+	jobStatus string
+}
+
+func (jobStatus *JobStatus) set(status string) {
+	jobStatus.mutex.Lock()
+	defer jobStatus.mutex.Unlock()
+	jobStatus.jobStatus = status
+}
+
+func (jobStatus *JobStatus) get() string {
+	jobStatus.mutex.RLock()
+	defer jobStatus.mutex.RUnlock()
+	return jobStatus.jobStatus
+}
+
+type ByteSlice struct {
+	mutex sync.Mutex
+	slice [][]byte
+}
+
+func (byteSlice *ByteSlice) append(sliceToAdd []byte) {
+	byteSlice.mutex.Lock()
+	defer byteSlice.mutex.Unlock()
+	byteSlice.slice = append(byteSlice.slice, sliceToAdd)
+}
+
+func (byteSlice *ByteSlice) get(index int) []byte {
+	byteSlice.mutex.Lock()
+	defer byteSlice.mutex.Unlock()
+	return byteSlice.slice[index]
+}
+
+func (byteSlice *ByteSlice) len() int {
+	byteSlice.mutex.Lock()
+	defer byteSlice.mutex.Unlock()
+	return len(byteSlice.slice)
+}
+
+func StartJob(processName string, parameters ...string) (*Job, error) { // consider returning id instead of a job
+	cmd := exec.Command(processName, parameters...)
+	stdout, stderr, err := getOutputPipesFromCmd(cmd)
+	if err != nil {
+		return nil, errors.New("can't get output from process")
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("job was failed to start")
+	}
+	job := &Job{JobStatus: JobStatus{jobStatus: InProgress}, ID: uuid.New(), Process: cmd.Process}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go captureOutput(stdout, &job.Output, &wg)
+	go captureOutput(stderr, &job.OutputErr, &wg)
+	go updateJobStatus(cmd, job, &wg)
+	return job, nil
+}
+
+// Please note, this function will terminate initiated process, but not its children.
+// I can either address the group termination in the next PR or we can put this out of scope.
+func StopJob(job *Job) (result bool, err error) {
+	err = job.Process.Kill()
+	if err != nil {
+		result = false
+	} else {
+		result = true
+	}
+	return
+}
+
+func GetJobOutput(job *Job, ch chan []byte, isStderr bool) {
+	var output *ByteSlice
+	if isStderr {
+		output = &job.OutputErr
+	} else {
+		output = &job.Output
+	}
+	var startRecordIndex int
+	if job.JobStatus.get() == InProgress {
+		startRecordIndex = getRealtimeOutput(job, ch, output)
+	}
+	getFinishedJobOutput(ch, output, startRecordIndex)
+	close(ch)
+
+}
+
+func getFinishedJobOutput(ch chan []byte, output *ByteSlice, startRecordIndex int) {
+	for i := startRecordIndex; i < output.len(); i++ {
+		ch <- output.get(i)
+	}
+}
+
+//Writes output to channel. Returns index of the next record to read
+func getRealtimeOutput(job *Job, ch chan []byte, output *ByteSlice) int {
+	i := 0
+	for job.JobStatus.get() == InProgress {
+		if output.len() > i {
+			ch <- output.get(i)
+			i++
+		}
+	}
+	return i
+}
+
+func captureOutput(output io.ReadCloser, writer *ByteSlice, wg *sync.WaitGroup) { // I don't even need to call wait in this case, since read will take care
+	defer wg.Done()
+	for {
+		buf := make([]byte, 1024)
+		n, err := output.Read(buf)
+		writer.append(buf[:n])
+		if err != nil {
+			if err != io.EOF {
+				log.Println(err)
+			}
+			break
+		}
+	}
+}
+
+func getOutputPipesFromCmd(cmd *exec.Cmd) (io.ReadCloser, io.ReadCloser, error) {
+	pipeFuncs := [2]func() (io.ReadCloser, error){cmd.StdoutPipe, cmd.StderrPipe}
+	stdPipes := make([]io.ReadCloser, 2)
+	for i, pipeFunc := range pipeFuncs {
+		var err error
+		stdPipes[i], err = pipeFunc()
+		if err != nil {
+			log.Println(err)
+			return nil, nil, err
+		}
+	}
+	return stdPipes[0], stdPipes[1], nil
+}
+
+func updateJobStatus(cmd *exec.Cmd, job *Job, wg *sync.WaitGroup) {
+	wg.Wait()
+	err := cmd.Wait()
+	if err != nil {
+		log.Println(fmt.Sprintf("Job ID '%v': %v", job.ID, err))
+	}
+	job.ExitCode = cmd.ProcessState.ExitCode()
+	job.JobStatus.set(getProcessStatusBasedOnCode(job.ExitCode))
+}
+
+func getProcessStatusBasedOnCode(exitCode int) string {
+	switch exitCode {
+	case 0:
+		return Exited
+	case -1:
+		return Terminated
+	default:
+		return ExitedWithErr
+	}
+
+}
---
+++ library/default_api.go
@@ -0,0 +1,97 @@
+package library
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+)
+
+/*
+This file contains the default implementation of library with user and job caching support.
+Basically it works as a little service, providing in memory storage and user support out of the box.
+/library/core should be used in case low level API is needed. In that case, it'll be pure job controlling API without user and job storage.
+*/
+
+var userJobContainer = NewUserJobsContainer()
+
+type UserJobsContainer struct {
+	userJobs map[string]map[string]*Job
+	mutex    sync.RWMutex
+}
+
+func NewUserJobsContainer() UserJobsContainer {
+	return UserJobsContainer{
+		userJobs: make(map[string]map[string]*Job),
+	}
+}
+
+func (userJobsContainer *UserJobsContainer) addJob(userName string, job *Job) {
+	userJobsContainer.mutex.Lock()
+	defer userJobsContainer.mutex.Unlock()
+	userJobs, ok := userJobsContainer.userJobs[userName]
+	jobId := job.ID.String()
+	if ok {
+		userJobs[jobId] = job
+	} else {
+		jobs := map[string]*Job{jobId: job} //make(map[string]*Job)
+		userJobsContainer.userJobs[userName] = jobs
+	}
+}
+
+func (userJobsContainer *UserJobsContainer) getJob(userName string, jobId string) (*Job, error) {
+	userJobsContainer.mutex.RLock()
+	defer userJobsContainer.mutex.RUnlock()
+	if userJobs, ok := userJobsContainer.userJobs[userName]; ok {
+		if job, ok := userJobs[jobId]; ok {
+			return job, nil
+		} else {
+			errorText := fmt.Sprintf("user doesn't have a job with id '%v'", jobId)
+			log.Println(errorText)
+			return nil, errors.New(errorText)
+		}
+	} else {
+		errorText := fmt.Sprintf("user %v doesn't exist", userName)
+		log.Println(errorText)
+		return nil, errors.New(errorText)
+	}
+}
+
+func StartUserJob(user string, processName string, parameters ...string) (string, error) {
+	job, err := StartJob(processName, parameters...)
+	if err != nil {
+		return "", err
+	}
+	userJobContainer.addJob(user, job)
+	return job.ID.String(), nil
+}
+
+func StopUserJob(user string, jobID string) (bool, error) {
+	if job, err := userJobContainer.getJob(user, jobID); err != nil {
+		return false, err
+	} else {
+		return StopJob(job)
+	}
+}
+
+func GetUserJobStatus(user string, jobID string) (jobStatus string, exitCode string, err error) {
+	if job, getJobErr := userJobContainer.getJob(user, jobID); getJobErr != nil {
+		err = getJobErr
+		return
+	} else {
+		if jobStatus = job.JobStatus.get(); jobStatus != InProgress {
+			exitCode = strconv.Itoa(job.ExitCode)
+		}
+		return
+	}
+}
+
+func GetUserJobOutput(user string, jobID string, ch chan []byte, isStdErr bool) error {
+	if job, err := userJobContainer.getJob(user, jobID); err != nil {
+		return err
+	} else {
+		GetJobOutput(job, ch, isStdErr)
+		return nil
+	}
+}
