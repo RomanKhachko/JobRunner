b6c70512d40dbe631593aa33e6ac0b55c1639881
Roman Khachko <romankhachko@gmail.com>
2021-10-13 10:57:39 -0500

    Add Godoc to export functions and types

--- library/core.go
+++ library/core.go
@@ -17,6 +17,7 @@ import (
 	"sync"
 )
 
+// Job status constants
 const (
 	InProgress    = "InProgress"
 	Exited        = "Exited"
@@ -24,27 +25,31 @@ const (
 	ExitedWithErr = "ExitedWithErr"
 )
 
+// Job stores the information about a job created by StartJob.
 type Job struct {
-	ID        uuid.UUID
-	Process   *os.Process
-	Output    ByteSlice
-	OutputErr ByteSlice
-	ExitCode  int
-	JobStatus JobStatus
+	ID        uuid.UUID   // unique ID of the job
+	process   *os.Process // underlying process associated with a job
+	output    ByteSlice   // contains output from stdout
+	outputErr ByteSlice   // contains output from stderr
+	ExitCode  int         // exit code of the underlying process
+	JobStatus JobStatus   // status of the job (one of InProgress, Exited, Terminated, ExitedWithErr)
 }
 
+// JobStatus represents status of the job.
 type JobStatus struct {
 	mutex     sync.RWMutex
 	jobStatus string
 }
 
-func (jobStatus *JobStatus) set(status string) {
+// Set sets the status of the job.
+func (jobStatus *JobStatus) Set(status string) {
 	jobStatus.mutex.Lock()
 	defer jobStatus.mutex.Unlock()
 	jobStatus.jobStatus = status
 }
 
-func (jobStatus *JobStatus) get() string {
+// Get returns the status of the job.
+func (jobStatus *JobStatus) Get() string {
 	jobStatus.mutex.RLock()
 	defer jobStatus.mutex.RUnlock()
 	return jobStatus.jobStatus
@@ -91,28 +96,33 @@ func (byteSlice *ByteSlice) waitForCondition() {
 	byteSlice.cond.Wait()
 }
 
+// StartJobs starts a requested process, starts job handler function running cuncurrently.
+// Once job is started, it will return. Returned Job contains all the relevent information about job.
+// If process couldn't be started or there were issues with getting output, error is returned.
 func StartJob(processName string, parameters ...string) (*Job, error) {
 	cmd := exec.Command(processName, parameters...)
 	stdout, stderr, err := getOutputPipesFromCmd(cmd)
 	if err != nil {
 		log.Println(err)
-		return nil, errors.New("can't get output from process")
+		return nil, errors.New("can't Get output from process")
 	}
 	err = cmd.Start()
 	if err != nil {
 		log.Println(err)
 		return nil, errors.New("job was failed to start")
 	}
-	job := &Job{JobStatus: JobStatus{jobStatus: InProgress}, ID: uuid.New(), Process: cmd.Process, Output: makeByteSlice(), OutputErr: makeByteSlice()}
+	job := &Job{JobStatus: JobStatus{jobStatus: InProgress}, ID: uuid.New(), process: cmd.Process, output: makeByteSlice(), outputErr: makeByteSlice()}
 	go handleJob(cmd, job, stdout, stderr)
 	return job, nil
 }
 
+// StopJob terminates the process associated with a job. Returns true in case of success, false and error otherwise.
 // Please note, this function will terminate initiated process, but not its children.
-// I can either address the group termination in the next PR or we can put this out of scope.
+// It can either be addressed the group termination in one of the next PRs or can be put out of scope.
 func StopJob(job *Job) (result bool, err error) {
-	err = job.Process.Kill()
+	err = job.process.Kill()
 	if err != nil {
+		log.Println(err)
 		result = false
 	} else {
 		result = true
@@ -120,15 +130,17 @@ func StopJob(job *Job) (result bool, err error) {
 	return
 }
 
+// GetJobOutput streams output of the job from the beginning until the job is completed.
+// Once output is returned, channel will be closed.
 func GetJobOutput(job *Job, ch chan<- []byte, isStderr bool) {
 	var output *ByteSlice
 	if isStderr {
-		output = &job.OutputErr
+		output = &job.outputErr
 	} else {
-		output = &job.Output
+		output = &job.output
 	}
 	var startRecordIndex int
-	if job.JobStatus.get() == InProgress {
+	if job.JobStatus.Get() == InProgress {
 		startRecordIndex = getRealtimeOutput(job, ch, output)
 	}
 	getFinishedJobOutput(ch, output, startRecordIndex)
@@ -145,7 +157,7 @@ func getFinishedJobOutput(ch chan<- []byte, output *ByteSlice, startRecordIndex 
 //Writes output to channel. Returns index of the next record to read
 func getRealtimeOutput(job *Job, ch chan<- []byte, output *ByteSlice) int {
 	i := 0
-	for job.JobStatus.get() == InProgress {
+	for job.JobStatus.Get() == InProgress {
 		if output.len() > i {
 			ch <- output.get(i)
 			i++
@@ -185,16 +197,16 @@ func updateJobStatus(cmd *exec.Cmd, job *Job) {
 		log.Println(fmt.Sprintf("Job ID '%v': %v", job.ID, err))
 	}
 	job.ExitCode = cmd.ProcessState.ExitCode()
-	job.JobStatus.set(getProcessStatusBasedOnCode(job.ExitCode))
-	job.OutputErr.broadcastCondition()
-	job.Output.broadcastCondition()
+	job.JobStatus.Set(getProcessStatusBasedOnCode(job.ExitCode))
+	job.outputErr.broadcastCondition()
+	job.output.broadcastCondition()
 }
 
 func handleJob(cmd *exec.Cmd, job *Job, stdout, stderr io.ReadCloser) {
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go captureOutput(stdout, &job.Output, &wg)
-	go captureOutput(stderr, &job.OutputErr, &wg)
+	go captureOutput(stdout, &job.output, &wg)
+	go captureOutput(stderr, &job.outputErr, &wg)
 	wg.Wait()
 	updateJobStatus(cmd, job)
 }
--- library/default_api.go
+++ library/default_api.go
@@ -1,3 +1,10 @@
+/*
+Package library implements manipulations with jobs. Supported operations: start, stop, output streaming, status.
+
+Library has two API:
+1) low level (core). It provides functions to manipulate jobs.
+2) high level (default). It wraps core functions and adds in memory storage and access control capabilities.
+*/
 package library
 
 import (
@@ -14,14 +21,14 @@ Basically it works as a little service, providing in memory storage and user sup
 /library/core should be used in case low level API is needed. In that case, it'll be pure job controlling API without user and job storage.
 */
 
-var userJobContainer = NewUserJobsContainer()
+var userJobContainer = makeUserJobsContainer()
 
 type UserJobsContainer struct {
 	userJobs map[string]map[string]*Job
 	mutex    sync.RWMutex
 }
 
-func NewUserJobsContainer() UserJobsContainer {
+func makeUserJobsContainer() UserJobsContainer {
 	return UserJobsContainer{
 		userJobs: make(map[string]map[string]*Job),
 	}
@@ -58,6 +65,8 @@ func (userJobsContainer *UserJobsContainer) getJob(userName string, jobId string
 	}
 }
 
+// StartUserJob initiates a job and associates it with a user requested it. Job id is returned when started successfully.
+// If job can't be started, error will be returned.
 func StartUserJob(user string, processName string, parameters ...string) (string, error) {
 	job, err := StartJob(processName, parameters...)
 	if err != nil {
@@ -67,6 +76,8 @@ func StartUserJob(user string, processName string, parameters ...string) (string
 	return job.ID.String(), nil
 }
 
+// StopUserJob terminates the job started by user. Returns true in case of success, false and error otherwise.
+// If user doesn't have any jobs or requested job doesn't belong to a user, false and error will be returned.
 func StopUserJob(user string, jobID string) (bool, error) {
 	if job, err := userJobContainer.getJob(user, jobID); err != nil {
 		return false, err
@@ -75,19 +86,24 @@ func StopUserJob(user string, jobID string) (bool, error) {
 	}
 }
 
+// GetUserJobStatus returns status of the job. If process exited, returns exit code as a second parameter.
+// If user doesn't have any jobs or requested job doesn't belong to a user, false and error will be returned.
 func GetUserJobStatus(user string, jobID string) (jobStatus string, exitCode string, err error) {
 	if job, getJobErr := userJobContainer.getJob(user, jobID); getJobErr != nil {
 		err = getJobErr
 		return
 	} else {
-		if jobStatus = job.JobStatus.get(); jobStatus != InProgress {
+		if jobStatus = job.JobStatus.Get(); jobStatus != InProgress {
 			exitCode = strconv.Itoa(job.ExitCode)
 		}
 		return
 	}
 }
 
-func GetUserJobOutput(user string, jobID string, ch chan []byte, isStdErr bool) error {
+// GetJobOutput streams output of the job from the beginning until the job is completed.
+// Once output is returned, channel will be closed.
+// If user doesn't have any jobs or requested job doesn't belong to a user, false and error will be returned.
+func GetUserJobOutput(user string, jobID string, ch chan<- []byte, isStdErr bool) error {
 	if job, err := userJobContainer.getJob(user, jobID); err != nil {
 		return err
 	} else {
